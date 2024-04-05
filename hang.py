7f1d2e9de64223b18f6c22c5f85e36d118cbdd77
hanitaddhh@gmail.com <hanitaddhh@gmail.com>
2024-04-05 18:18:18 +0430

    questions

--- hang.py
+++ hang.py
@@ -1,1 +1,11 @@
-print("something else")
+import random
+
+questions = [{
+    'title': "food",
+    "item" :["PIZZA","PASTA","SPAGHETTI"]}, 
+{ 'title': "carModel",
+    "item" : ["CADILLAC","TOYOTA","BMW"]},
+    { 'title': "fruit",
+    "item" : ["MANGO","APPLE","CHERRY"]}
+]
+
