1d6fa072a13c10b45f2518b9c847ade6e3ae767e
hanitaddhh@gmail.com <hanitaddhh@gmail.com>
2024-04-08 12:19:25 +0430

    guess the letter

--- hang.py
+++ hang.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 questions = [{
     'title': "food",
     "item" :["PIZZA","PASTA","SPAGHETTI"]}, 
@@ -11,5 +13,21 @@ questions = [{
 
 randomTitle = random.choice(questions)
 randomItem = random.choice(randomTitle["item"])
-print(randomTitle["title"])
-print(randomItem)
+# print(randomTitle["title"])
+# print(randomItem)
+message = ""
+for i in range(len(randomItem)):
+    message += "_" + " "
+while True:
+    print("guess the letter")
+    print(message)
+    userGuess = input("what is the {} word?".format(randomTitle["title"]))
+  
+
+    for i in userGuess:
+        if i.upper() in randomItem:
+            print(True)
+        else:
+            print(False)
+
+
